321a2aa0a2450037107246fd1dc0367eb418f089
ImmGot <raffael.proenca@gmail.com>
2025-03-21 14:02:28 -0300

    Improved code responsiveness

--- js/form.js
+++ js/form.js
@@ -1,21 +1,21 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
- 
+
 botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
- 
+
     var form = document.querySelector("#form-adiciona");
- 
+
     var paciente = obtemPacienteDoFormulario(form);
- 
+
     var pacienteTr = montaTr(paciente);
- 
+
     var tabela = document.querySelector("#tabela-pacientes");
- 
+
     tabela.appendChild(pacienteTr);
- 
+
     form.reset();
 })
- 
+
 function obtemPacienteDoFormulario(form) {
     var paciente = {
         nome: form.nome.value,
@@ -26,22 +26,59 @@ function obtemPacienteDoFormulario(form) {
     }
     return paciente;
 }
- 
+
 function montaTd(dado) {
     var td = document.createElement("td");
     td.textContent = dado;
- 
+
     return td;
 }
- 
+
 function montaTr(paciente) {
     var pacienteTr = document.createElement("tr");
- 
+
     pacienteTr.appendChild(montaTd(paciente.nome));
     pacienteTr.appendChild(montaTd(paciente.peso));
     pacienteTr.appendChild(montaTd(paciente.altura));
     pacienteTr.appendChild(montaTd(paciente.gordura));
     pacienteTr.appendChild(montaTd(paciente.imc));
- 
+
     return pacienteTr;
+}
+
+function validaPaciente(paciente) {
+    var erros = [];
+
+    if (paciente.nome.length == 0) {
+        erros.push("Nome é obrigatório!");
+    }
+    if (paciente.altura.length == 0) {
+        erros.push("Altura é obrigatória!");
+    }
+    if (paciente.peso.length == 0) {
+        erros.push("Peso é obrigatório!");
+    }
+    if (paciente.gordura.length == 0) {
+        erros.push("Gordura é obrigatória!");
+    }
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido!");
+    }
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida!");
+    }
+
+    return erros;
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#msg-erro");
+
+    erros.forEach(erro => {
+
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+
 }
